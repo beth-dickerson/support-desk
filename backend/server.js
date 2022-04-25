675cfc54148a84511a8295cc4ddcdfb14cad6e20
Beth Dickerson <c.dickerson@elsevier.com>
2022-04-25 11:44:11 -0400

    updating with front end. submit function no worky

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,26 @@
-console.log('Server...');
+const express = require("express");
+const colors = require("colors");
+const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connnectDB = require("./config/db");
+const PORT = process.env.PORT || 8000;
+
+// Connect to database
+connnectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Welcome to the Suppport Desk API" });
+});
+
+// Routes
+app.use("/api/users", require("./routes/userRoutes"));
+app.use("/api/tickets", require("./routes/ticketRoutes"));
+
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
